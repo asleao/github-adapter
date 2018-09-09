15b881a307f51269c292897e058bc0b9d6138ac4
André Spadetto Leão <andre.sp.leao@gmail.com>
2018-09-09 20:46:52 -0300

    __init.py

--- githubms/__init__.py
+++ githubms/__init__.py
@@ -1,38 +1,25 @@
 import os
 
 from flask import Flask
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='TdiKTNTzjx3iYIYbHSZAbw==',
+    DATABASE=os.path.join(app.instance_path, 'githubms.sqlite')
+)
 
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='TdiKTNTzjx3iYIYbHSZAbw==',
-        DATABASE=os.path.join(app.instance_path, 'githubms.sqlite')
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
-    from . import github
-    app.register_blueprint(github.blueprint)
-    app.add_url_rule('/v1/auth', endpoint='authenticate')
-
-    return app
+from githubms import github
+app.register_blueprint(github.blueprint)
+app.add_url_rule('/v1/auth', endpoint='authenticate')
 
 
 # Configuration for deploy on Heroku
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
