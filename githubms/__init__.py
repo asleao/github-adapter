7ba7e38cc4e8d30af4e87adf6d24125efae787b4
André Spadetto Leão <andre.sp.leao@gmail.com>
2018-09-09 20:40:13 -0300

    Test.

--- githubms/__init__.py
+++ githubms/__init__.py
@@ -33,5 +33,6 @@ def create_app(test_config=None):
 
 # Configuration for deploy on Heroku
 if __name__ == "__main__":
+    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
