163fa91ed919f907e565201c6cb158957de908f9
André Spadetto Leão <andre.sp.leao@gmail.com>
2018-10-21 17:15:26 -0300

    Heroku config.

--- app/__init__.py
+++ app/__init__.py
@@ -24,3 +24,8 @@ from .github.controller import blueprint
 app.register_blueprint(blueprint)
 app.add_url_rule('/v1/repo', endpoint='repository')
 app.add_url_rule('/v1/repo/collaborators', endpoint='repository/collaborators')
+
+# Configuration for deploy on Heroku
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
