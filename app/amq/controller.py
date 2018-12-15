c9eaee085d15062cd35cc10d39292e60825452f4
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2018-12-15 13:09:12 -0200

    getenv.

--- app/amq/controller.py
+++ app/amq/controller.py
@@ -2,11 +2,14 @@ import pika
 import os
 import logging
 import json
+
+from dotenv.environ import getenv
+
 from app.github import controller
 
 
 def setup():
-    URL = os.environ.get('CLOUDAMQP_URL')
+    URL = getenv('CLOUDAMQP_URL')
     return pika.URLParameters(URL)
 
 
