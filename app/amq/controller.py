00599f713b50b3b6fd81da34cbdefb8a8f7ff3a3
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-07-30 23:02:41 -0300

    Rollback problems with last release.

--- app/amq/controller.py
+++ app/amq/controller.py
@@ -1,31 +1,21 @@
 import json
+import os
 
 import pika
-from dotenv.environ import getenv
 
 from app.github import controller
-from app.models.repository_data import RepositoryData
 
 
 def setup():
-    URL = getenv('CLOUDAMQP_URL')
+    URL = os.environ.get('CLOUDAMQP_URL')
     return pika.URLParameters(URL)
 
 
 def callback_repository(ch, method, properties, body):
     body = json.loads(body.decode('utf8').replace("'", '"'))
-    repository_data = RepositoryData(body['name'],
-                                     body['action'],
-                                     body['token'],
-                                     body['language'])
-    controller.repository(repository_data)
+    controller.repository(body)
 
 
 def callback_collaborator(ch, method, properties, body):
     body = json.loads(body.decode('utf8').replace("'", '"'))
-    collaborator_data = RepositoryData(body['name'],
-                                       body['action'],
-                                       body['token'],
-                                       body['language'])
-    collaborator_data.collaborators = body['collaborators']
-    controller.manage_collaborators(collaborator_data)
+    controller.manage_collaborators(body)
--- app/github/controller.py
+++ app/github/controller.py
@@ -1,37 +1,53 @@
-"""
-    Class responsable for dealing with the github api requests.
-"""
 from github import Github
 
-from app.models.repository_data import RepositoryData
+
+def authenticate(data):
+    if 'token' in data:
+        return Github(data['token'])
+    elif 'username' and 'password' in data:
+        return Github(data['username'], data['password'])
 
 
-def repository(repository_data: RepositoryData):
+def repository(data):
     """
     Funcion responsable for getting the credentials and create a repository.
     """
-    github_object = Github(repository_data.token)
-    language = repository_data.language
-    repository_name = repository_data.repository_name
-    repository = github_object.get_user().create_repo(
-        repository_name, gitignore_template=language, auto_init=True)
-    print('{} created succesfully!'.format(repository_name))
+    github_object = authenticate(data=data)
+    language = data['language']
+    repository_name = data['name']
+    if data['action'] == 'add':
+        add_repository(github_object, language, repository_name)
+    elif data['action'] == 'remove':
+        username = github_object.get_user().login
+        repository_slug = '{}/{}'.format(username, repository_name)
+        repo = github_object.get_repo(repository_slug)
+        if repo is not None:
+            repo.delete()
+            print('{} removed succesfully!'.format(repository_name))
 
     # TODO Enviar callback?
 
 
-def manage_collaborators(repository_data: RepositoryData):
+def add_repository(github_object, language, repository_name):
+    github_object.get_user().create_repo(
+        repository_name, gitignore_template=language, auto_init=True)
+    print('{} created succesfully!'.format(repository_name))
+
+
+def manage_collaborators(data):
     """
         Funcion responsable for manage collaborators of the repository.
     """
-    github_object = Github(repository_data.token)
-    repository = github_object.get_user().get_repo(repository_data.repository_name)
+    github_object = authenticate(data=data)
+    collaborators = data['collaborators']
+    repository_name = data['name']
+    repository = github_object.get_user().get_repo(repository_name)
 
-    for collaborator in repository_data.collaborators:
-        if repository_data.action == 'add':
-            add_collaborator(repository, collaborator, repository_data.repository_name)
-        elif repository_data.action == 'remove':
-            remove_collaborator(repository, collaborator, repository_data.repository_name)
+    for collaborator in collaborators:
+        if data['action'] == 'add':
+            add_collaborator(repository, collaborator, repository_name)
+        elif data['action'] == 'remove':
+            remove_collaborator(repository, collaborator, repository_name)
 
 
 def add_collaborator(repository, collaborator, repository_name):
@@ -48,7 +64,6 @@ def remove_collaborator(repository, collaborator, repository_name):
         Funcion responsable to remove collaborators from the repository.
     """
     if not repository.has_in_collaborators(collaborator):
-        # TODO: realizar um retorno http para usuário não existente
         return print('{} doesn\'t exist in {}!'.format(collaborator, repository_name))
     else:
         repository.remove_from_collaborators(collaborator)
